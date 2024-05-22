2983d4a9d26906fe5fc2ab694865956d58fc3e39
perfectaeon <andrewgwozd3221@gmail.com>
2024-05-22 17:56:38 +0600

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://muchbackendwork.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://muchbackendwork.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
